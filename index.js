822d2d88a016db9f260f81df0ee17e63243e288f
Ciarán <bubonicfred@gmail.com>
2020-12-27 08:32:35 +1000

    Tyding up and regex work

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 /** @babel */
 /* global atom:true */
 
-const lint = require('./lint');
+const lint = require('./lint')
 
-let activated = false;
+let activated = false
 
 export const activate = () => {
-  activated = true;
-};
+  activated = true
+}
 
 export const deactivate = () => {
-  activated = false;
-};
+  activated = false
+}
 
 export const provideLinter = () => ({
   name: 'British',
@@ -19,17 +19,17 @@ export const provideLinter = () => ({
   lintsOnChange: true,
   grammarScopes: ['*'],
   lint: async textEditor => {
-    if (!activated) return [];
+    if (!activated) return []
 
-    if (atom.inDevMode()) console.time('lint-british'); // eslint-disable-line no-console
+    if (atom.inDevMode()) console.time('lint-british') // eslint-disable-line no-console
 
-    const editorPath = textEditor.getPath();
-    const editorText = textEditor.buffer.getText();
+    const editorPath = textEditor.getPath()
+    const editorText = textEditor.buffer.getText()
 
-    const results = lint(editorPath, editorText);
+    const results = lint(editorPath, editorText)
 
-    if (atom.inDevMode()) console.timeEnd('lint-british'); // eslint-disable-line no-console
+    if (atom.inDevMode()) console.timeEnd('lint-british') // eslint-disable-line no-console
 
-    return results;
-  },
-});
+    return results
+  }
+})
--- lint.js
+++ lint.js
@@ -1,30 +1,29 @@
-const list = require('./list');
+const list = require('./list')
 
 const createMatchObjectMerger = matchObject => (start, end) => {
-  const newMatchObject = matchObject;
-  newMatchObject.location.position[0][1] = start;
-  newMatchObject.location.position[1][1] = end;
-  return newMatchObject;
-};
-
+  const newMatchObject = matchObject
+  newMatchObject.location.position[0][1] = start
+  newMatchObject.location.position[1][1] = end
+  return newMatchObject
+}
 const createMatchFinder = merger => (contents, regex) => {
-  let matches = [];
+  let matches = []
   for (;;) {
-    const match = regex.exec(contents);
-    if (match === null) break;
-    matches = matches.concat(merger(match.index, match.index + match[0].length));
+    const match = regex.exec(contents)
+    if (match === null) break
+    matches = matches.concat(merger(match.index, match.index + match[0].length))
   }
-  return matches;
-};
+  return matches
+}
 
 const reduceDecorator = callback => [
   (accumulator, currentValue, currentIndex, array) => {
-    const result = callback(accumulator, currentValue, currentIndex, array);
-    if (Array.isArray(result) && result.length === 0) return accumulator;
-    return [...accumulator, ...result];
+    const result = callback(accumulator, currentValue, currentIndex, array)
+    if (Array.isArray(result) && result.length === 0) return accumulator
+    return [...accumulator, ...result]
   },
-  [],
-];
+  []
+]
 
 const findSpelling = file => (allLineMatches, lineContents, lineNumber) => (
   list.reduce(...reduceDecorator((allMatches, { british, american }) => {
@@ -32,16 +31,16 @@ const findSpelling = file => (allLineMatches, lineContents, lineNumber) => (
       severity: 'info',
       location: {
         file,
-        position: [[lineNumber, 0], [lineNumber, 1]],
+        position: [[lineNumber, 0], [lineNumber, 1]]
       },
       excerpt: `You should spell this "${british}"!`,
-      description: '> _This is American._ ',
-    });
-    const matchFinder = createMatchFinder(merger);
-    return matchFinder(lineContents, new RegExp(american, 'gi'));
+      description: '> _This is American._ '
+    })
+    const matchFinder = createMatchFinder(merger)
+    return matchFinder(lineContents, new RegExp('\\b' + american + '\\b', 'gi'))
   }))
-);
+)
 
 module.exports = (filePath, fileContents) => (
   fileContents.split('\n').reduce(...reduceDecorator(findSpelling(filePath)))
-);
+)
